fcf75b651ca536195b99ff3265b4eb5629bf86de
Cosmin <cosminnegrut776@gmail.com>
2025-08-20 03:53:12 +0300

    replace productName to productId for a cleaner code

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -51,7 +51,7 @@ products.forEach((product) => {
       </div>
 
       <button class="add-to-cart-button button-primary js-add-to-cart"
-      data-product-name='${product.name}'
+      data-product-id='${product.id}'
       >
         Add to Cart
       </button>
@@ -64,11 +64,11 @@ document.querySelector('.products-grid').innerHTML=productsHTML
 
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener('click',()=>{
-    const productName=button.dataset.productName;
+    const productId=button.dataset.productId;
     let matchingItem;
 
     cart.forEach((item)=>{
-      if(productName===item.productName){
+      if(productId===item.productId){
         matchingItem= item
       }
     })
@@ -76,7 +76,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
         matchingItem.quantity+=1;
       }else{
          cart.push({
-      productName:productName,
+      productId:productId,
       quantity:1
     })
 
