cde72a706c8896c331161c63b4e83f9dd68f89ad
Cosmin <cosminnegrut776@gmail.com>
2025-09-17 04:36:57 +0300

    split functions and organise code better using modules

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,6 @@
 
+import {cart, addToCart} from '../data/cart.js'
+import {products} from '../data/products.js'
 
 
  
@@ -62,34 +64,24 @@ products.forEach((product) => {
 
 document.querySelector('.products-grid').innerHTML=productsHTML
 
-document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
-  button.addEventListener('click',()=>{
-    const productId=button.dataset.productId;
-    let matchingItem;
 
-    cart.forEach((item)=>{
-      if(productId===item.productId){
-        matchingItem= item
-      }
-    })
-      if(matchingItem){
-        matchingItem.quantity+=1;
-      }else{
-         cart.push({
-      productId:productId,
-      quantity:1
-    })
-
-      }
-      let cartQuantity=0;
-      cart.forEach((item)=>{
-        cartQuantity+=item.quantity
+function updateCartQuantity(){
+   let cartQuantity=0;
+      cart.forEach((cartItem)=>{
+        cartQuantity+=cartItem.quantity
       })
       document.querySelector('.js-cart-quantity').innerHTML= cartQuantity
       console.log(cartQuantity);
     console.log(cart);
+}
+
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+  button.addEventListener('click',()=>{
+ const productId=button.dataset.productId;
+   addToCart(productId)
+   updateCartQuantity()
+     
   })
-  
 })
 
 
