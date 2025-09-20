b500ffa306e9587d7b471ba1201e94931157bd27
Negrut Cosmin <cosminnegrut776@gmail.com>
2025-09-20 23:59:34 +0300

    generate html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,18 @@
-import {cart} from '../data/cart';
-import { products } from '../data/products';
+import {cart} from '../data/cart.js';
+import {products} from '../data/products.js'
+
 
 cart.forEach((cartItem)=>{
   const productId=cartItem.productId
+
+  let matchingProduct
+  products.forEach((product)=>{
+    if(product.id===productId){
+      matchingProduct=product
+    }
+  })
+
+ 
 `
     <div class="cart-item-container">
       <div class="delivery-date">
@@ -10,18 +20,18 @@ cart.forEach((cartItem)=>{
       </div>
 
       <div class="cart-item-details-grid">
-        <img class="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg">
+        <img class="product-image" src="${matchingProduct.image}">
 
         <div class="cart-item-details">
           <div class="product-name">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+            ${matchingProduct.name}
           </div>
           <div class="product-price">
-            $10.90
+            ${matchingProduct.priceCents / 100}
           </div>
           <div class="product-quantity">
             <span>
-              Quantity: <span class="quantity-label">2</span>
+              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary">
               Update
